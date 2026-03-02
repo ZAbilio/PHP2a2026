91d3f76876b171a1c09b9940a165c83d965b281a
enzo abilio <zabilio006@gmail.com>
2026-03-02 20:06:25 -0300

    atividades

--- aula.php
+++ aula.php
@@ -5,8 +5,8 @@
 
 <body>
     <?php
-    $1 = 1;
-    while ($1 < 6){
+   /* $i = 1;
+    while ($i < 6){
         echo $i;
         $i = $i++;
     } 
@@ -15,7 +15,22 @@
     $cor = array("vermelho","verde","blue", "yellow");
     foreach ($cor as $valor){
         echo "$valor  <br>"
+    }*/
+
+
+    /*atv1*/
+    echo "atividade 1"
+    $num = array (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+    foreach($num= %2==0){
+         echo "$num" . "<br>",
     }
+
+     /*atv2*/
+     $var1=
+     $var2=                                      
+     $var3=
+     
+
 ?>
 </body>
 </html>
